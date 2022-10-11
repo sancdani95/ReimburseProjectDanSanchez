d7ed5c9fc211451995a4169c15a3d3871bf5fb6e
Daniel Sanchez <daniel759@revature.net>
2022-10-11 15:00:32 -0600

    first project update

---
+++ ReimburseProjectDanSanchez/src/main/java/com/revature/Driver.java
@@ -0,0 +1,44 @@
+package com.revature;
+
+import org.eclipse.jetty.http.HttpStatus;
+
+import com.revature.model.Person;
+
+import io.javalin.Javalin;
+
+public class Driver {
+	
+	//ctx.res().getWriter().write("Please enter your username and password.");
+	
+	//Person receivedPerson = ctx.bodyAsClass(Person.class);
+			//System.out.println(receivedPerson);
+			//ctx.status(HttpStatus.CREATED_201);
+	
+	public static void main(String[] args) {
+		
+		//starts server on 8000 port
+		Javalin app = Javalin.create().start(8000);
+		
+		//receives login request
+		app.post("/login", ctx -> {
+			Person receivedPerson = ctx.bodyAsClass(Person.class);
+			if (receivedPerson.getUsername() == ){ //SQL check here)
+				// get userid and then use it to get corrisponding password
+				if (receivedPerson.getPassword() == ) {//check SQL password
+					//send approved login message
+				}
+				else //send password does not match message
+					
+		}
+			else // send username does not match
+			
+		});
+		
+		
+		//request employee past tickets from SQl databace
+		app.get("/employee", ctx -> {	
+		
+		});
+	}
+
+}
